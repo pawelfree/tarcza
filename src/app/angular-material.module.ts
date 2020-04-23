1dd512670925617aa25633462fb25027c99f5d99
Paweł Dudek <paweldudek@Pawes-MacBook-Pro.local>
2020-04-23 05:39:40 +0200

    kolor pocztowego

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
     exports: [
         MatCardModule,
         MatProgressSpinnerModule,
-        MatToolbarModule]
+        MatToolbarModule,
+        MatDividerModule]
 })
 export class AngularMaterialModule { }
