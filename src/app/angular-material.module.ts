e229e93b52c0a9c593fa7c505ca1e956adc5159c
Pawel Rzewuski <hrabia@Hrabia.local>
2020-04-23 17:01:06 +0200

    Zabawa

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+
+
+
 
 @NgModule({
     exports: [
         MatCardModule,
         MatProgressSpinnerModule,
         MatToolbarModule,
-        MatDividerModule]
+        MatDividerModule,
+        MatButtonModule,
+        MatGridListModule,
+        MatIconModule,
+        MatTableModule]
 })
 export class AngularMaterialModule { }
