8ed4ae6ed12050169fe396bb88d87b6e2977bf26
Pawel Rzewuski <hrabia@Hrabia.local>
2020-04-23 21:57:32 +0200

    funkcje wyswietlania

--- src/app/wnioski/wnioski.component.ts
+++ src/app/wnioski/wnioski.component.ts
@@ -10,10 +10,44 @@ import { WnioskiService } from '../services/wnioski.service';
 })
 export class WnioskiComponent implements OnInit {
 
+  
   wnioski$: Observable<Wniosek[]>;
 
   constructor(private wnioski : WnioskiService) { }
 
+  statusColor(applicationStatus:string) {
+    var statusClass:string="";
+
+    switch(applicationStatus) {
+      case "Złożony" : { 
+        statusClass="green";
+        break;
+      }
+      case "Wysłany" : { 
+          statusClass="green";
+          break;
+      }
+      case "Odrzucony" : { 
+          statusClass="red";
+        break;
+      } default : {
+        break;
+      }
+    }
+    return statusClass
+  }
+
+  showAppeal(applicationStatus:string, amountReq:number, amountGranded:number) {
+      var hiddenAppeal = false;
+      hiddenAppeal = amountGranded != amountReq ? false : true; 
+
+      return hiddenAppeal
+  
+  
+  }
+
+  
+    
   ngOnInit(): void {
     this.wnioski$ = this.wnioski.wszystkieWnioski();
   }
