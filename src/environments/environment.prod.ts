3f4498ce60854d292e2a41adcda2ac8ebb9a1ddf
HrabiaBurbon <p.rzewuski@gmail.com>
2020-04-26 10:45:58 +0200

    Dodanie adrsu API PROD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://tarcza.qa.bp.com.pl/api/",
+  apiUrl: "https://tarcza.pocztowy.pl/api/", //PRODUKCJA
+  //apiUrl: "https://tarcza.qa.bp.com.pl/api/", //QA
   getDocumentMethod: "QUERY" //PARAM
 };
