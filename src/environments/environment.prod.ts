0bebe679a3d55ad056c0c4e83d4998c3a24a5c4f
HrabiaBurbon <p.rzewuski@gmail.com>
2020-04-26 13:20:05 +0200

    konfiguracja

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  //apiUrl: "https://tarcza.pocztowy.pl/api/", //PRODUKCJA
-  apiUrl: "https://tarcza.qa.bp.com.pl/api/", //QA
+  //apiUrl: "https://tarcza.pocztowy.pl/api/", //PRODUKCJA - klient zewnętrzny
+  apiUrl: "https://tarcza.bp.com.pl/api/", //PRODUKCJA - klient wewnętrzny
+  //apiUrl: "https://tarcza.qa.bp.com.pl/api/", //QA
   getDocumentMethod: "QUERY" //PARAM
 };
