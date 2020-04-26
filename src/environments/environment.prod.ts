b1c0001495d48c7f5a730c195924ad90746f2ca1
HrabiaBurbon <p.rzewuski@gmail.com>
2020-04-26 16:53:04 +0200

    location host as enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
+  apiUrl: location.host + '/api/',
   //apiUrl: "https://tarcza.pocztowy.pl/api/", //PRODUKCJA - klient zewnętrzny
-  apiUrl: "https://tarcza.bp.com.pl/api/", //PRODUKCJA - klient wewnętrzny
+  //apiUrl: "https://tarcza.bp.com.pl/api/", //PRODUKCJA - klient wewnętrzny
   //apiUrl: "https://tarcza.qa.bp.com.pl/api/", //QA
   getDocumentMethod: "QUERY" //PARAM
 };
