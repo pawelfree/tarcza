642bcd15b61462542b60030318251162e8e63845
HrabiaBurbon <p.rzewuski@gmail.com>
2020-05-06 11:30:56 +0200

    Dodanie daty decyzji (zmiana modelu, zmiana danych w lokalnym serwerze, zmiana componentów i stylów wyświetlania)

--- apps/tarcza/src/app/models/wniosek.ts
+++ apps/tarcza/src/app/models/wniosek.ts
@@ -3,6 +3,7 @@ export interface Wniosek {
     userName: string;
     companyName: string;
     applicationDateRequested: string;
+    applicationDateDecision: string;
     applicationStatus: string;
     amountRequested: number;
     amountGranted: number;
