a1d995a2427f283fa24424a0ca5e1a3482eefb8c
HrabiaBurbon <p.rzewuski@gmail.com>
2020-05-06 21:13:52 +0200

    srodowisko produkcyjne - sprzątanie

--- apps/tarcza/src/environments/environment.prod.ts
+++ apps/tarcza/src/environments/environment.prod.ts
@@ -1,8 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: "/api/",
-  //apiUrl: "https://tarcza.pocztowy.pl/api/", //PRODUKCJA - klient zewnętrzny
-  //apiUrl: "https://tarcza.bp.com.pl/api/", //PRODUKCJA - klient wewnętrzny
-  //apiUrl: "https://tarcza.qa.bp.com.pl/api/", //QA
   getDocumentMethod: "QUERY" //PARAM
 };
